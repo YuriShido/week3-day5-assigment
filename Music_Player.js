13f82925fcb0d53c5c4c186b60286b0b6ba4e2b6
YuriShido <sydonce57@gmail.com>
2020-08-22 19:34:14 -0700

    Done

--- Music_Player.js
+++ Music_Player.js
@@ -32,38 +32,63 @@ let library = {
 
 
   printAllSongNames: function() {
-    // print the names of all the songs to the console
+    let songIDs = Object.keys(library.songs)
+    console.log(songIDs)
+    for(i = 0; i < songIDs.length; i++){
+     console.log(library.songs[ songIDs[i] ].name) 
+    }
+    
   },
 
   printSongName: function(songID) {
+    console.log(library.songs[songID].name)
     // print the name of a song when given its ID
   },
 
   printPlaylistName: function(playlistID) {
+    console.log(library.playlists[playlistID].name)
     // Print the name of a playlist when given its ID 
   },
 
   printAllPlaylistNames: function() {
+    let listIDs = Object.keys(library.playlists)
+    for(x = 0; x < listIDs.length; x++){
+      console.log(library.playlists[listIDs[x]].name)
+    }
     // Print the nmaes of all the playlists 
   },
 
 
   printPlaylistSongs: function(playlistID) {
+    console.log(library.playlists[playlistID].name)
     // Print the names of all the songs in whatever playlist id was given
   },
 
   addSong: function(name, artist, album) {
+    
+    let newSong =  {id:library.generateUid(),name:name, artist:artist, album:album};
+    library.songs["s04"] = newSong;
+    console.log(library.songs)
+  
+    
     // add a new song to the songs object. The song should be its own object, 
     // containing a randomly generated id, a name, an artist, and an album 
     // console.log to confirm that the song has been added.
   },
 
   addSongToPlaylist: function(songID, playlistID) {
+    library.playlists[playlistID].tracks.push(songID)
+    console.log(library.playlists)
     // given a songID, add that song to the playlist for the given playlistID
     //console.log the playlist to make sure the song was added.
   },
 
   addPlaylist: function(name, arrOfSongs) {
+    let newPlay = {id:library.generateUid(), name:name, tracks:arrOfSongs};
+    library.playlists["p03"] = newPlay;
+    console.log(library.playlists)
+
+
     // add a new playlist to the playlist object. it will be
     // containing a randomly generated id, a name, and an array of songs to be added to the playlist 
     // console.log to confirm that the playlist has been added.
@@ -75,11 +100,26 @@ let library = {
 
 }
 
-
-
 // console.log(Object.keys(library.songs))
 
 // library.addSongToPlaylist('s02', 'p02');
 
 // console.log(library.songs);
 
+// (library.printAllSongNames());
+
+// library.printSongName("s03")
+
+// library.printPlaylistName('p01');
+
+// console.log(Object.keys(library.playlists))
+
+// library.printAllPlaylistNames();
+
+// library.printPlaylistSongs('p01')
+
+// library.addSong('Smile', 'Sydonce', 'Happy')
+
+// library.addSongToPlaylist('s04', 'p01')
+
+library.addPlaylist('Love song', ['s57'])
---
+++ try.js
@@ -0,0 +1,15 @@
+var myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+console.log(myCar)
+
+addSong: function(name, artist, album) {
+  let newSong = library.songs.push(id{'name', 'artist', 'album'})
+  console.log(newSong)
+
+
+  let newSong = new Object
+  newSong.name = 
+  library.songs.push(id{'name', 'artist', 'album'})
+  console.log(newSong)
